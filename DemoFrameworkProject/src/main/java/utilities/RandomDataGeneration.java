3f68fc5d9e1888ba9fc75cc3224c2e28745ae997
iamha <iamha@HAREESHMA-SARATH-IRISH>
2024-08-04 13:07:41 +0530

    new update chaining

--- DemoFrameworkProject/src/main/java/utilities/RandomDataGeneration.java
+++ DemoFrameworkProject/src/main/java/utilities/RandomDataGeneration.java
@@ -8,10 +8,12 @@ public class RandomDataGeneration {
 
 	static Faker faker;
 	static String gname;
+	static int price;
 
 	public static String getGivenName() {
 		faker = new Faker(new Locale("en-IND"));
 		gname=faker.name().fullName();
+		price=faker.number().numberBetween(500, 2000);
 		return gname;
 	}
 }
