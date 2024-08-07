b6d182cbc6a7cd8d13ac28699b02420bf737e434
iamha <iamha@HAREESHMA-SARATH-IRISH>
2024-08-07 20:38:21 +0530

    new updates

--- DemoFrameworkProject/src/test/java/testClasses/LoginTestClass.java
+++ DemoFrameworkProject/src/test/java/testClasses/LoginTestClass.java
@@ -22,7 +22,7 @@ public class LoginTestClass extends BaseClass {
 		/*
 		 * chaining
 		 */
-		lp.typeOnUsername(username).typeOnPassword(password).clickOnElement().clickOnButton();
+		(new LoginPageClass(driver)).typeOnUsername(username).typeOnPassword(password).clickOnElement().clickOnButton();
 		
 		/*
 		 * chaing end
@@ -32,33 +32,30 @@ public class LoginTestClass extends BaseClass {
 		Assert.assertEquals(actual_result, lp.readStringData(3, 0));
 	}
 
-	@Test(dataProviderClass = DataProviderClass.class, dataProvider = "unsuccessful",priority = 3,groups = {"groupOne"},retryAnalyzer = RetryAnalyzer.class)
-	public void verifyUnsuccessfulLogin(String userName,String Password) throws IOException {
-		lp = new LoginPageClass(driver);
-		//lp.login(lp.readStringData(1, 3), lp.readStringData(1, 4));
-		lp.login(userName, Password);
-		String actual_Result = lp.getTextOfErrorMessageFromLoginPage();
-		Assert.assertEquals(actual_Result, lp.readStringData(3, 3));
-	}
-
-	@Test(priority = 4,groups = {"groupTwo"},retryAnalyzer = RetryAnalyzer.class)
-	public void veryfyLogoIsDisplayedOrNotInLoginPage() {
-		lp = new LoginPageClass(driver);
-		boolean logo_status = lp.isLogoDisplayed();
-		Assert.assertTrue(logo_status, "Logo Not Found");
-	}
-
-	@Test(priority = 1,groups = {"groupTwo"},retryAnalyzer = RetryAnalyzer.class)
-	public void verifyTheExpectedPageisOpensWhileHittingTheUrl() {
-		lp = new LoginPageClass(driver);
-		String actual_result = lp.getTextOfCurrentLoginPageURL();
-		Assert.assertEquals(actual_result, "LOGIN");
-	}
-
-	@Test(priority = 5,groups = {"groupTwo"},retryAnalyzer = RetryAnalyzer.class)
-	public void verifyAllLocationSessionsAreDisplayedOrNotInLoginPage() {
-		lp = new LoginPageClass(driver);
-		boolean actual_result = lp.isAllSessionsAreDisplayed();
-		Assert.assertEquals(actual_result, true);
-	}
+	/*
+	 * @Test(dataProviderClass = DataProviderClass.class, dataProvider =
+	 * "unsuccessful",priority = 3,groups = {"groupOne"},retryAnalyzer =
+	 * RetryAnalyzer.class) public void verifyUnsuccessfulLogin(String
+	 * userName,String Password) throws IOException { lp = new
+	 * LoginPageClass(driver); //lp.login(lp.readStringData(1, 3),
+	 * lp.readStringData(1, 4)); lp.login(userName, Password); String actual_Result
+	 * = lp.getTextOfErrorMessageFromLoginPage(); Assert.assertEquals(actual_Result,
+	 * lp.readStringData(3, 3)); }
+	 * 
+	 * @Test(priority = 4,groups = {"groupTwo"},retryAnalyzer = RetryAnalyzer.class)
+	 * public void veryfyLogoIsDisplayedOrNotInLoginPage() { lp = new
+	 * LoginPageClass(driver); boolean logo_status = lp.isLogoDisplayed();
+	 * Assert.assertTrue(logo_status, "Logo Not Found"); }
+	 * 
+	 * @Test(priority = 1,groups = {"groupTwo"},retryAnalyzer = RetryAnalyzer.class)
+	 * public void verifyTheExpectedPageisOpensWhileHittingTheUrl() { lp = new
+	 * LoginPageClass(driver); String actual_result =
+	 * lp.getTextOfCurrentLoginPageURL(); Assert.assertEquals(actual_result,
+	 * "LOGIN"); }
+	 * 
+	 * @Test(priority = 5,groups = {"groupTwo"},retryAnalyzer = RetryAnalyzer.class)
+	 * public void verifyAllLocationSessionsAreDisplayedOrNotInLoginPage() { lp =
+	 * new LoginPageClass(driver); boolean actual_result =
+	 * lp.isAllSessionsAreDisplayed(); Assert.assertEquals(actual_result, true); }
+	 */
 }
